4d4bc4ad3f7138d1e40404fac0ef0a174b97d2fd
Rohit <rohitsharmatech9@gmail.com>
2022-12-29 17:56:43 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(Configuration);
+console.log(process.env.OPENAI_API_KEY);
+
+
+app.get('/', async (req,res)=>{
+    res.status(200).send({
+        Message: "Hello World from Qshell",
+
+    });
+});
+
+app.post('/', async (req, res)=>{
+   try{
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+    });
+    res.status(200).send({
+        bot: response.data.choices[0].text
+    });
+   } 
+   catch(error){
+    console.log(error);
+    res.status(500).send({error});
+   }
+
+});
+
+app.listen(8080,() => {
+    console.log("server is running on port http://localhost:8080");
+});
